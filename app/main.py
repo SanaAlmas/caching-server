72132bf5c0516ab91b695e95b12b776c9ab6033a
Sans <sanaalmas19@gmail.com>
2026-01-02 14:15:48 +0530

    Accepting on a port and starting flask server

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 import argparse
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 def start_server():
     parser = argparse.ArgumentParser(description="caching-proxy")
     parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
-    parser.add_argument("--origin", default=".", help="Directory to serve")
+    parser.add_argument("--origin", default="localhost/products", help="Directory to serve")
     args = parser.parse_args()
+    app.run(host="127.0.0.1", port= args.port)
     print(f"Proxy starting on port {args.port}")
     print(f"Origin: {args.origin}")
